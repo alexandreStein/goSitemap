7b2b8d0f90222bd68bcc4ec28ced3ebd8677e082
Alexandre Stein <astin58@gmail.com>
2015-09-02 10:54:40 +0200

    Sitemap operationnal

--- mainSitemap.go
+++ mainSitemap.go
@@ -1,26 +1,63 @@
 package sitemap
 
-import "time"
+import (
+	"encoding/xml"
+	"time"
+)
 
 const (
 	headerXmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
 )
 
-// SitemapContainer is the page
-type Container struct {
-	Urlset []*SitemapList `xml:"urlset"`
-	Xmlns  string         `xml:"xmlns,attr"`
+// Page is the XML page
+type Page struct {
+	XMLName xml.Name   `xml:"urlset"`
+	Urlset  []*Sitemap `xml:"url"`
+	Xmlns   string     `xml:"xmlns,attr"`
 }
 
-// // SitemapList is only a name older tu make it conform to the sitemap structure
-// type SitemapList struct {
-// 	Map *Sitemap `xml:"url"`
-// }
+// GetXML return the corresponding sitemap XML
+func (c *Page) GetXML() ([]byte, error) {
+	c.Xmlns = headerXmlns
+
+	retXML, err := xml.Marshal(c)
+	// retXML, err := xml.MarshalIndent(c, "", "	")
+	if err != nil {
+		return nil, err
+	}
+
+	ret := []byte(xml.Header)
+	ret = append(ret, retXML...)
+
+	return ret, nil
+}
+
+// IndexPage represent the sitemap index element
+type IndexPage struct {
+	XMLName xml.Name   `xml:"sitemapindex"`
+	Urlset  []*Sitemap `xml:"sitemap"`
+	Xmlns   string     `xml:"xmlns,attr"`
+}
+
+// GetXML return the corresponding sitemap XML
+func (c *IndexPage) GetXML() ([]byte, error) {
+	c.Xmlns = headerXmlns
+
+	retXML, err := xml.Marshal(c)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := []byte(xml.Header)
+	ret = append(ret, retXML...)
+
+	return ret, nil
+}
 
 // Sitemap is actualy the real sitemap element
 type Sitemap struct {
-	Loc        string
-	LastMod    time.Time
-	Changefreq string
-	Priority   float32
+	Loc        string     `xml:"loc"`
+	LastMod    *time.Time `xml:"lastmod,omitempty"`
+	Changefreq string     `xml:"changefreq,omitempty"`
+	Priority   float32    `xml:"priority,omitempty"`
 }
